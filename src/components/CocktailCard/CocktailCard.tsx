870980077ca6539ffe09a9efe76a768adddc8a78
martin <martinmeneghetti@outlook.it>
2021-12-06 17:58:15 +0100

    modified image size of cocktails in search page

--- src/components/CocktailCard/CocktailCard.tsx
+++ src/components/CocktailCard/CocktailCard.tsx
@@ -4,26 +4,22 @@ import Image from "../Image/Image";
 import "./CocktailCard.scss";
 
 interface CocktailCardProps extends RouteComponentProps {
-    id: string;
-    name: string;
-    img: string;
+  id: string;
+  name: string;
+  img: string;
 }
 
 const CocktailCard: FC<CocktailCardProps> = (props) => {
-    const goToCocktailPage = () => {
-        props.history.push(`/cocktails/${props.id}`);
-    };
+  const goToCocktailPage = () => {
+    props.history.push(`/cocktails/${props.id}`);
+  };
 
-    return (
-        <div className="cocktail-card" onClick={goToCocktailPage}>
-            <Image
-                src={props.img + "/preview"}
-                alt="cocktail"
-                className="cocktail-card__img"
-            />
-            <span className="cocktail-card__label">{props.name}</span>
-        </div>
-    );
+  return (
+    <div className="cocktail-card" onClick={goToCocktailPage}>
+      <Image src={props.img} alt="cocktail" className="cocktail-card__img" />
+      <span className="cocktail-card__label">{props.name}</span>
+    </div>
+  );
 };
 
 export default withRouter(CocktailCard);
--- src/containers/FullCocktail/FullCocktail.tsx
+++ src/containers/FullCocktail/FullCocktail.tsx
@@ -12,145 +12,137 @@ import LanguageSelector from "../../components/LanguageSelector/LanguageSelector
 import "./FullCocktail.scss";
 
 const FullCocktail: FC = () => {
-    const cocktails = useSelector(
-        (state: RootState) => state.cocktails.cocktails
-    );
-    const selectedLanguage = useSelector(
-        (state: RootState) => state.ui.selectedLanguage
-    );
-    const { id } = useParams() as any;
-    const [selectedCocktail, setSelectedCocktail] = useState<any>(null);
+  const cocktails = useSelector(
+    (state: RootState) => state.cocktails.cocktails
+  );
+  const selectedLanguage = useSelector(
+    (state: RootState) => state.ui.selectedLanguage
+  );
+  const { id } = useParams() as any;
+  const [selectedCocktail, setSelectedCocktail] = useState<any>(null);
 
-    useEffect(() => {
-        (async () => {
-            if (cocktails && cocktails.hasOwnProperty(id)) {
-                setSelectedCocktail(cocktails[id]);
-            } else {
-                const response = await axios.get(`/lookup.php?i=${id}`);
-                setSelectedCocktail(response.data.drinks[0]);
-            }
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      if (cocktails && cocktails.hasOwnProperty(id)) {
+        setSelectedCocktail(cocktails[id]);
+      } else {
+        const response = await axios.get(`/lookup.php?i=${id}`);
+        setSelectedCocktail(response.data.drinks[0]);
+      }
+    })();
+  }, []);
 
-    const getTags = () => {
-        return selectedCocktail.strTags
-            ? selectedCocktail.strTags.split(",")
-            : [];
-    };
-    const getIngredients = useCallback(() => {
-        let ingredients = [];
-        for (let i = 1; i <= 15; i++) {
-            if (!selectedCocktail[`strIngredient${i}`]) break;
-            ingredients.push({
-                baseName: selectedCocktail[`strIngredient${i}`],
-                fullName:
-                    (selectedCocktail[`strMeasure${i}`] || "") +
-                    " " +
-                    (selectedCocktail[`strIngredient${i}`] || ""),
-                imageUrl: `https://www.thecocktaildb.com/images/ingredients/${
-                    selectedCocktail[`strIngredient${i}`]
-                }-Medium.png`,
-            });
-        }
-        return ingredients;
-    }, [selectedCocktail]);
-    const getLanguageInstructions = () => {
-        switch (selectedLanguage) {
-            case Language.English:
-                return selectedCocktail.strInstructions || "Not available";
-            case Language.Italian:
-                return selectedCocktail.strInstructionsIT || "Not available";
-            case Language.Spanish:
-                return selectedCocktail.strInstructionsES || "Not available";
-            case Language.French:
-                return selectedCocktail.strInstructionsFR || "Not available";
-            case Language.Germany:
-                return selectedCocktail.strInstructionsDE || "Not available";
-            default:
-                return selectedCocktail.strInstructions;
-        }
-    };
+  const getTags = () => {
+    return selectedCocktail.strTags ? selectedCocktail.strTags.split(",") : [];
+  };
+  const getIngredients = useCallback(() => {
+    let ingredients = [];
+    for (let i = 1; i <= 15; i++) {
+      if (!selectedCocktail[`strIngredient${i}`]) break;
+      ingredients.push({
+        baseName: selectedCocktail[`strIngredient${i}`],
+        fullName:
+          (selectedCocktail[`strMeasure${i}`] || "") +
+          " " +
+          (selectedCocktail[`strIngredient${i}`] || ""),
+        imageUrl: `https://www.thecocktaildb.com/images/ingredients/${
+          selectedCocktail[`strIngredient${i}`]
+        }-Medium.png`,
+      });
+    }
+    return ingredients;
+  }, [selectedCocktail]);
+  const getLanguageInstructions = () => {
+    switch (selectedLanguage) {
+      case Language.English:
+        return selectedCocktail.strInstructions || "Not available";
+      case Language.Italian:
+        return selectedCocktail.strInstructionsIT || "Not available";
+      case Language.Spanish:
+        return selectedCocktail.strInstructionsES || "Not available";
+      case Language.French:
+        return selectedCocktail.strInstructionsFR || "Not available";
+      case Language.Germany:
+        return selectedCocktail.strInstructionsDE || "Not available";
+      default:
+        return selectedCocktail.strInstructions;
+    }
+  };
 
-    return (
-        <main className="app__page full-cocktail">
-            {selectedCocktail ? (
-                <Fragment>
-                    <h1 className="full-cocktail__title">
-                        {selectedCocktail.strDrink}
-                    </h1>
-                    <div className="full-cocktail__container">
-                        <div className="full-cocktail__left-side">
-                            <Image
-                                src={selectedCocktail.strDrinkThumb}
-                                alt="cocktail"
-                                className="full-cocktail__left-side__img"
-                            />
-                            <div className="full-cocktail__left-side__category">
-                                <span className="full-cocktail__left-side__category__label">
-                                    CATEGORY:
-                                </span>
-                                <span className="full-cocktail__left-side__category__text">
-                                    {selectedCocktail.strCategory}
-                                </span>
-                            </div>
-                            <div className="full-cocktail__left-side__tags">
-                                {getTags().map((tag: string) => (
-                                    <span
-                                        className="full-cocktail__left-side__tags__tag"
-                                        key={tag}
-                                    >
-                                        # {tag}
-                                    </span>
-                                ))}
-                            </div>
-                        </div>
-                        <div className="divider"></div>
-                        <div className="full-cocktail__right-side">
-                            <h2 className="full-cocktail__right-side__title">
-                                INGREDIENTS:
-                            </h2>
-                            <div className="full-cocktail__right-side__ingredients">
-                                {getIngredients().map((ingredient) => (
-                                    <div
-                                        key={ingredient.baseName}
-                                        className="full-cocktail__right-side__ingredients__ingredient"
-                                    >
-                                        <Image
-                                            src={ingredient.imageUrl}
-                                            alt={ingredient.baseName}
-                                            className="full-cocktail__right-side__ingredients__ingredient__image"
-                                        />
-                                        <span className="full-cocktail__right-side__ingredients__ingredient__label">
-                                            {ingredient.fullName}
-                                        </span>
-                                    </div>
-                                ))}
-                            </div>
-                            <div className="full-cocktail__right-side__glass">
-                                <span className="full-cocktail__right-side__title">
-                                    GLASS:
-                                </span>
-                                <span className="full-cocktail__right-side__glass__glass-name">
-                                    {selectedCocktail.strGlass}
-                                </span>
-                            </div>
-                            <div className="full-cocktail__right-side__instructions">
-                                <div className="full-cocktail__right-side__title">
-                                    <span>INSTRUCTIONS:</span>
-                                </div>
-                                <LanguageSelector />
-                                <p className="full-cocktail__right-side__instructions__text">
-                                    {getLanguageInstructions()}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </Fragment>
-            ) : (
-                <Spinner />
-            )}
-        </main>
-    );
+  return (
+    <main className="app__page full-cocktail">
+      {selectedCocktail ? (
+        <Fragment>
+          <h1 className="full-cocktail__title">{selectedCocktail.strDrink}</h1>
+          <div className="full-cocktail__container">
+            <div className="full-cocktail__left-side">
+              <Image
+                src={selectedCocktail.strDrinkThumb}
+                alt="cocktail"
+                className="full-cocktail__left-side__img"
+              />
+              <div className="full-cocktail__left-side__category">
+                <span className="full-cocktail__left-side__category__label">
+                  CATEGORY:
+                </span>
+                <span className="full-cocktail__left-side__category__text">
+                  {selectedCocktail.strCategory}
+                </span>
+              </div>
+              <div className="full-cocktail__left-side__tags">
+                {getTags().map((tag: string) => (
+                  <span
+                    className="full-cocktail__left-side__tags__tag"
+                    key={tag}
+                  >
+                    # {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="divider"></div>
+            <div className="full-cocktail__right-side">
+              <h2 className="full-cocktail__right-side__title">INGREDIENTS:</h2>
+              <div className="full-cocktail__right-side__ingredients">
+                {getIngredients().map((ingredient) => (
+                  <div
+                    key={ingredient.baseName}
+                    className="full-cocktail__right-side__ingredients__ingredient"
+                  >
+                    <Image
+                      src={ingredient.imageUrl}
+                      alt={ingredient.baseName}
+                      className="full-cocktail__right-side__ingredients__ingredient__image"
+                    />
+                    <span className="full-cocktail__right-side__ingredients__ingredient__label">
+                      {ingredient.fullName}
+                    </span>
+                  </div>
+                ))}
+              </div>
+              <div className="full-cocktail__right-side__glass">
+                <span className="full-cocktail__right-side__title">GLASS:</span>
+                <span className="full-cocktail__right-side__glass__glass-name">
+                  {selectedCocktail.strGlass}
+                </span>
+              </div>
+              <div className="full-cocktail__right-side__instructions">
+                <div className="full-cocktail__right-side__title">
+                  <span>INSTRUCTIONS:</span>
+                </div>
+                <LanguageSelector />
+                <p className="full-cocktail__right-side__instructions__text">
+                  {getLanguageInstructions()}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      ) : (
+        <Spinner />
+      )}
+    </main>
+  );
 };
 
 export default FullCocktail;
